2e872b7c182969a67495e29d395d1e3868f9e674
Andrew Davies <davies.andrew.123@gmail.com>
2024-03-10 15:20:13 -0700

    Updated camera controller to handle camera rotation

--- Assets/Scripts/PlayerControllers/CameraController.cs
+++ Assets/Scripts/PlayerControllers/CameraController.cs
@@ -1,11 +1,19 @@
-﻿using UnityEngine;
+﻿using Unity.VisualScripting;
+using UnityEngine;
 
 namespace PlayerControllers
 {
 	public class CameraController : MonoBehaviour
 	{
 		[SerializeField] private Camera playerCamera;
-		[SerializeField] private float cameraSpeed = 1f;
+		
+		[Header("Speeds")]
+		[SerializeField] private float cameraMoveSpeed = 1f;
+		[SerializeField] private float cameraRotationSpeed = 1f;
+
+		[Header("Clamps")]
+		[SerializeField] private float minVerticalRotation = 45f;
+		[SerializeField] private float maxVerticalRotation = 90f;
 
 		private Transform _cameraTransform;
 
@@ -17,30 +25,51 @@ namespace PlayerControllers
 		private void Update()
 		{
 			ControlPosition();
+			ControlRotation();
 		}
 
 		private void ControlPosition()
 		{
 			float xMovement = 0f;
 			if (Input.GetKey(KeyCode.A))
-				xMovement -= Time.deltaTime * cameraSpeed;
+				xMovement -= 1;
 			if (Input.GetKey(KeyCode.D))
-				xMovement += Time.deltaTime * cameraSpeed;
-			
+				xMovement += 1;
 			
 			float zMovement = 0f;
 			if (Input.GetKey(KeyCode.S))
-				zMovement -= Time.deltaTime * cameraSpeed;
+				zMovement -= 1;
 			if (Input.GetKey(KeyCode.W))
-				zMovement += Time.deltaTime * cameraSpeed;
+				zMovement += 1;
 
 			float yMovement = 0f;
-			if (Input.GetKey(KeyCode.Space) && Input.GetKey(KeyCode.LeftShift))
-				yMovement -= Time.deltaTime * cameraSpeed;
-			if (Input.GetKey(KeyCode.Space) && !Input.GetKey(KeyCode.LeftShift))
-				yMovement += Time.deltaTime * cameraSpeed;
+			if (Input.GetKey(KeyCode.LeftShift))
+				yMovement -= 1;
+			if (Input.GetKey(KeyCode.Space))
+				yMovement += 1;
+
+			Vector3 cameraForward = _cameraTransform.forward;
 			
-			_cameraTransform.Translate(new Vector3(xMovement, yMovement, zMovement), Space.Self);
+			Vector3 xVector = _cameraTransform.right * xMovement;
+			Vector3 yVector = Vector3.up * yMovement;
+			Vector3 zVector = new Vector3(cameraForward.x, 0, cameraForward.z).normalized * zMovement;
+
+			_cameraTransform.position += (xVector + yVector + zVector).normalized * (Time.deltaTime * cameraMoveSpeed);
+		}
+
+		private float _currentVerticalRotation;
+		private float _currentHorizontalRotation;
+
+		private void ControlRotation()
+		{
+			if (!Input.GetMouseButton((int) MouseButton.Middle))
+				return;
+
+			_currentHorizontalRotation += Input.GetAxisRaw("Mouse X") * cameraRotationSpeed;
+			_currentVerticalRotation -= Input.GetAxisRaw("Mouse Y") * cameraRotationSpeed;
+			_currentVerticalRotation = Mathf.Clamp(_currentVerticalRotation, minVerticalRotation, maxVerticalRotation);
+
+			_cameraTransform.eulerAngles = new Vector3(_currentVerticalRotation, _currentHorizontalRotation, 0f);
 		}
 	}
 }
