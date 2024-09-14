f6940ed54ec39f1d34e0592c50f5a9aaf3a9cf16
Andrew Davies <davies.andrew.123@gmail.com>
2024-09-13 19:10:53 -0700

    Added an attack state for wizards

---
+++ Assets/Scripts/Wizards/States/AttackWizardState.cs
@@ -0,0 +1,64 @@
+﻿using GameWorld;
+using System;
+using UnityEngine;
+
+namespace Wizards.States
+{
+	public class AttackWizardState : WizardState
+	{
+		public const string EXIT_REASON_TARGET_DEAD = nameof(EXIT_REASON_TARGET_DEAD);
+		public const string EXIT_REASON_TARGET_OUT_OF_RANGE = nameof(EXIT_REASON_TARGET_OUT_OF_RANGE);
+
+		private float _attackTimer = 0f;
+
+		public override event EventHandler<string> ExitRequested;
+
+		public AttackWizardState(Wizard wizard) : base(wizard) { }
+
+		public override string DisplayName => "Attacking";
+		public override string DisplayStatus { get; protected set; }
+
+		public Character Target { get; set; }
+		public float AttackRadius { get; set; }
+		public float TimeBetweenAttacks { get; set; }
+		public float AttackDamage { get; set; }
+
+		private bool CanAttack => _attackTimer >= TimeBetweenAttacks;
+
+		public void Initialize(Character target)
+		{
+			Target = target;
+		}
+
+		public override void Begin() 
+		{
+			// Able to do their first attack right away
+			_attackTimer = TimeBetweenAttacks;
+		}
+
+		public override void End() { }
+
+		public override void Update() 
+		{
+			if (!Target)
+			{
+				ExitRequested?.Invoke(this, EXIT_REASON_TARGET_DEAD);
+				return;
+			}
+
+			if (Vector3.Distance(Wizard.Position, Target.Position) > AttackRadius)
+			{
+				ExitRequested?.Invoke(this, EXIT_REASON_TARGET_OUT_OF_RANGE);
+				return;
+			}
+
+			_attackTimer += Time.deltaTime;
+
+			if (CanAttack)
+			{
+				Target.Health.Health.CurrentHealth -= AttackDamage;
+				_attackTimer -= TimeBetweenAttacks;
+			}
+		}
+	}
+}
--- Assets/Scripts/Wizards/WizardStateMachine.cs
+++ Assets/Scripts/Wizards/WizardStateMachine.cs
@@ -1,4 +1,5 @@
-﻿using GeneralBehaviours;
+﻿using Enemies;
+using GeneralBehaviours;
 using StateMachines;
 using System;
 using UnityEngine;
@@ -16,6 +17,7 @@ namespace Wizards
 		private WizardIdleState _idleState;
 		private WizardMoveToState _moveToState;
 		private RunAwayWizardState _runAwayState;
+		private AttackWizardState _attackState;
 
 		public void OverrideCurrentState(WizardState state)
 		{
@@ -41,6 +43,7 @@ namespace Wizards
 			_idleState = new WizardIdleState(_wizard);
 			_moveToState = new WizardMoveToState(_wizard);
 			_runAwayState = new RunAwayWizardState(_wizard, 10f);
+			_attackState = new AttackWizardState(_wizard);
 
 			StateMachine.DefaultState = _idleState;
 		}
@@ -51,6 +54,14 @@ namespace Wizards
 
 			StateMachine.AddStateTransition(
 				new StateTransitionKey(_runAwayState, RunAwayWizardState.EXIT_REASON_GOT_AWAY),
+				new StateTransitionValue(_idleState, null, () => true));
+
+			StateMachine.AddStateTransition(
+				new StateTransitionKey(_attackState, AttackWizardState.EXIT_REASON_TARGET_DEAD),
+				new StateTransitionValue(_idleState, null, () => true));
+
+			StateMachine.AddStateTransition(
+				new StateTransitionKey(_attackState, AttackWizardState.EXIT_REASON_TARGET_OUT_OF_RANGE),
 				new StateTransitionValue(_idleState, null, () => true));
 		}
 
@@ -66,7 +77,19 @@ namespace Wizards
 
 		private void OnWizardHurt(object sender, EventArgs args)
 		{
-			StateMachine.SetCurrentState(_runAwayState);
+			if (!StateMachine.IsCurrentState(_runAwayState) && !StateMachine.IsCurrentState(_attackState))
+			{
+				if (UnityEngine.Random.Range(0, 2) == 0)
+					StateMachine.SetCurrentState(_runAwayState);
+				else
+				{
+					_attackState.AttackDamage = 5;
+					_attackState.AttackRadius = 2f;
+					_attackState.TimeBetweenAttacks = 1f;
+					_attackState.Target = FindFirstObjectByType<Enemy>(); // Todo: Find a way to get the enemy that is attacking them
+					StateMachine.SetCurrentState(_attackState);
+				}
+			}
 		}
 	}
 }
