fdd71bd929d3342dcc5cc61dbbec62ad91f1843d
Andrew Davies <davies.andrew.123@gmail.com>
2024-09-17 18:34:10 -0700

    Added speed text to the World Time UI

--- Assets/Scripts/UI/WorldTimeUI/WorldTimeUI.cs
+++ Assets/Scripts/UI/WorldTimeUI/WorldTimeUI.cs
@@ -14,26 +14,37 @@ namespace UI.WorldTimeUI
 		[SerializeField] private float updatesPerSecond;
 
 		[Header("Time Buttons")]
-		[SerializeField, Required] private Button pauseButton;
+		[SerializeField, Required] private Button pauseSpeedButton;
 		[SerializeField, Required] private Button normalSpeedButton;
 		[SerializeField, Required] private Button doubleSpeedButton;
 		[SerializeField, Required] private Button quadrupleSpeedButton;
+		[SerializeField, Required] private TMP_Text speedText;
 		[SerializeField] private Color selectedColor;
 		[SerializeField] private Color unselectedColor;
 
 		private Button _selectedButton;
 		private float _waitTime = 0f;
 
+		private TimeScaleGroup _pausedSpeedGroup;
+		private TimeScaleGroup _normalSpeedGroup;
+		private TimeScaleGroup _doubleSpeedGroup;
+		private TimeScaleGroup _quadrupleSpeedGroup;
+
 		public bool UpdateText { get; set; } = true;
 
 		private void Awake()
 		{
 			_waitTime = 1.0f / updatesPerSecond;
+
+			_pausedSpeedGroup = new TimeScaleGroup(pauseSpeedButton, 0f, "0x");
+			_normalSpeedGroup = new TimeScaleGroup(normalSpeedButton, 1f, "1x");
+			_doubleSpeedGroup = new TimeScaleGroup(doubleSpeedButton, 2f, "2x");
+			_quadrupleSpeedGroup = new TimeScaleGroup(quadrupleSpeedButton, 4f, "4x");
 		}
 
 		private void Start()
 		{
-			pauseButton.onClick.AddListener(OnPauseButtonPressed);
+			pauseSpeedButton.onClick.AddListener(OnPauseButtonPressed);
 			normalSpeedButton.onClick.AddListener(OnNormalSpeedButtonPressed);
 			doubleSpeedButton.onClick.AddListener(OnDoubleSpeedButtonPressed);
 			quadrupleSpeedButton.onClick.AddListener(OnQuadrupleSpeedButtonPressed);
@@ -46,7 +57,7 @@ namespace UI.WorldTimeUI
 		{
 			StopAllCoroutines();
 
-			pauseButton.onClick.RemoveListener(OnPauseButtonPressed);
+			pauseSpeedButton.onClick.RemoveListener(OnPauseButtonPressed);
 			normalSpeedButton.onClick.RemoveListener(OnNormalSpeedButtonPressed);
 			doubleSpeedButton.onClick.RemoveListener(OnDoubleSpeedButtonPressed);
 			quadrupleSpeedButton.onClick.RemoveListener(OnQuadrupleSpeedButtonPressed);
@@ -61,21 +72,36 @@ namespace UI.WorldTimeUI
 			}
 		}
 
-		private void OnPauseButtonPressed() => OnButtonSelected(0f, pauseButton);
-		private void OnNormalSpeedButtonPressed() => OnButtonSelected(1f, normalSpeedButton);
-		private void OnDoubleSpeedButtonPressed() => OnButtonSelected(2f, doubleSpeedButton);
-		private void OnQuadrupleSpeedButtonPressed() => OnButtonSelected(4f, quadrupleSpeedButton);
+		private void OnPauseButtonPressed() => SetCurrentTimeScaleGroup(_pausedSpeedGroup);
+		private void OnNormalSpeedButtonPressed() => SetCurrentTimeScaleGroup(_normalSpeedGroup);
+		private void OnDoubleSpeedButtonPressed() => SetCurrentTimeScaleGroup(_doubleSpeedGroup);
+		private void OnQuadrupleSpeedButtonPressed() => SetCurrentTimeScaleGroup(_quadrupleSpeedGroup);
 
-		private void OnButtonSelected(float timeMultiplier, Button selectedButton)
+		private void SetCurrentTimeScaleGroup(TimeScaleGroup group)
 		{
-			Time.timeScale = timeMultiplier;
-			worldTime.TimeMultiplier = timeMultiplier;
+			Time.timeScale = group.Scale;
+			worldTime.TimeMultiplier = group.Scale;
+			speedText.SetText(group.ScaleText);
 
 			if (_selectedButton)
 				_selectedButton.image.color = unselectedColor;
-			_selectedButton = selectedButton;
+			_selectedButton = group.Button;
 			if (_selectedButton)
 				_selectedButton.image.color = selectedColor;
+		}
+
+		private class TimeScaleGroup
+		{
+			public Button Button { get; }
+			public float Scale { get; }
+			public string ScaleText { get; }
+
+			public TimeScaleGroup(Button button, float scale, string scaleText)
+			{
+				Button = button;
+				Scale = scale;
+				ScaleText = scaleText;
+			}
 		}
 	}
 }
