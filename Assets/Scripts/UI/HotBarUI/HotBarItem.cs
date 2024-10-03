13c1583bc97060b05523850bfec9603509c5eced
Andrew Davies <davies.andrew.123@gmail.com>
2024-10-03 01:27:55 -0700

    Added UI Updates

--- Assets/Scripts/UI/HotBarUI/HotBarItem.cs
+++ Assets/Scripts/UI/HotBarUI/HotBarItem.cs
@@ -1,16 +1,17 @@
 ﻿using System;
-using Extensions;
 using GameWorld.WorldObjects;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Utilities.Attributes;
 
 namespace UI.HotBarUI
 {
 	public class HotBarItem : MonoBehaviour
 	{
-		[SerializeField] private Button button;
-		[SerializeField] private WorldObjectDetails worldObjectDetails;
-
+		[SerializeField, Required] private Button button;
+		[SerializeField, Required] private WorldObjectDetails worldObjectDetails;
+		
 		public event EventHandler<HotBarItemSelectedEventArgs> Selected;
 
 		public WorldObjectDetails WorldObjectDetails => worldObjectDetails;
@@ -18,16 +19,36 @@ namespace UI.HotBarUI
 		public void Enable() => button.interactable = true;
 		public void Disable() => button.interactable = false;
 		
-		private void Awake()
+		private void Start()
 		{
-			button.ThrowIfNull(nameof(button));
-			worldObjectDetails.ThrowIfNull(nameof(worldObjectDetails));
 			button.onClick.AddListener(OnButtonClicked);
+			InitializeButtonText();
+		}
+
+		private void OnValidate()
+		{
+			if (worldObjectDetails)
+			{
+				gameObject.name = $"{worldObjectDetails.Name} Hot Bar Item";
+				if (button)
+				{
+					InitializeButtonText();
+				}
+			}
+			else
+			{
+				gameObject.name = "Hot Bar Item";
+			}
 		}
 
 		private void OnDestroy() => button.onClick.RemoveListener(OnButtonClicked);
 		private void OnButtonClicked() => Selected?.Invoke(this, new HotBarItemSelectedEventArgs(worldObjectDetails));
 
+		private void InitializeButtonText()
+		{
+			button.GetComponentInChildren<TMP_Text>().SetText(worldObjectDetails.Name);
+		}
+		
 		public class HotBarItemSelectedEventArgs : EventArgs
 		{
 			public HotBarItemSelectedEventArgs(WorldObjectDetails worldObjectDetails)
