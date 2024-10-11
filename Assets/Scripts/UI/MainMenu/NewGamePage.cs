63fc853c5ded2a7d748d6d55215198de0a95feef
Andrew Davies <davies.andrew.123@gmail.com>
2024-10-11 10:00:34 -0700

    Updated new game page to validate the inputs and enable/disable the create button

--- Assets/Scripts/UI/MainMenu/NewGamePage.cs
+++ Assets/Scripts/UI/MainMenu/NewGamePage.cs
@@ -1,10 +1,10 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Utilities.Attributes;
 
 namespace UI.MainMenu
 {
-	// Todo: Disable create button until all input fields are filled out
 	// Todo: Create an object to store all world creation variables in
 	// Todo: Start the gameplay scene
 	// Todo: Create the world
@@ -16,25 +16,44 @@ namespace UI.MainMenu
 	{
 		[SerializeField, Required] private TMP_InputField worldNameInput;
 		[SerializeField, Required] private TMP_InputField worldSeedInput;
+		[SerializeField, Required] private Button createButton;
 
 		private MainMenuUIPage _uiPage;
+
+		private bool IsWorldNameValid => worldNameInput.text.Length > 0;
+		private bool IsWorldSeedValid => worldSeedInput.text.Length > 0;
 		
 		private void OnPageEnabled()
 		{
 			worldNameInput.SetTextWithoutNotify(string.Empty);
 			worldSeedInput.SetTextWithoutNotify(string.Empty);
+			ValidateAllInputs();
+
+			worldNameInput.onValueChanged.AddListener(_ => ValidateAllInputs());
+			worldSeedInput.onValueChanged.AddListener(_ => ValidateAllInputs());
 		}
+
+		private void OnPageDisabled()
+		{
+			worldNameInput.onValueChanged.RemoveAllListeners();
+			worldSeedInput.onValueChanged.RemoveAllListeners();
+		}
+
+		private void ValidateAllInputs() => 
+			createButton.interactable = IsWorldNameValid && IsWorldSeedValid;
 
 		private void Awake()
 		{
 			_uiPage = GetComponent<MainMenuUIPage>();
 
 			_uiPage.PageEnabled += OnPageEnabled;
+			_uiPage.PageDisabled += OnPageDisabled;
 		}
 
 		private void OnDestroy()
 		{
 			_uiPage.PageEnabled -= OnPageEnabled;
+			_uiPage.PageDisabled -= OnPageDisabled;
 		}
 	}
 }
