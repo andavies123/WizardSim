a88a143fbd293f4bdb916f7fc888d6eefec1358c
Andrew Davies <davies.andrew.123@gmail.com>
2025-02-04 22:52:16 -0800

    Added a few more colors to the Tile generation

--- Assets/Scripts/GameWorld/Tiles/Tile.cs
+++ Assets/Scripts/GameWorld/Tiles/Tile.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections.Generic;
+using GeneralBehaviours.ShaderManagers;
 using UI;
 using UI.ContextMenus;
 using UnityEngine;
+using Utilities.Attributes;
 
 namespace GameWorld.Tiles
 {
 	[RequireComponent(typeof(Interactable))]
+	[RequireComponent(typeof(InteractionShaderManager))]
 	public class Tile : MonoBehaviour, IContextMenuUser
 	{
+		[SerializeField, Required] private List<Color> colors;
+		
 		private Interactable _interactable;
+		private InteractionShaderManager _shaderManager;
 		
 		public World ParentWorld { get; private set; }
 		public Chunk ParentChunk { get; private set; }
@@ -24,12 +30,14 @@ namespace GameWorld.Tiles
 			gameObject.name = $"Tile - {tilePosition}";
 			
 			InitializeInteractable();
+			_shaderManager.OverrideBaseColor(colors[Random.Range(0, colors.Count)]);
 		}
 
 		private void Awake()
 		{
 			Transform = transform;
 			_interactable = GetComponent<Interactable>();
+			_shaderManager = GetComponent<InteractionShaderManager>();
 		}
 
 		private void InitializeInteractable()
